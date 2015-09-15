bfa7334da793d4b3fe7e678e61df8f641d33ca8e
delight24326 <delight24326@gmail.com>
2015-09-15 20:50:36 +0900

    First Canvas

---
+++ imageCanvas.js
@@ -0,0 +1,7 @@
+function drawScreen() {
+    var theCanvas = document.getElementById("GameCanvas");
+    var Context = theCanvas.getContext("2d");
+    Context.fillStyle = "#000000";
+    Context.fillRect(0, 0, 1000, 700);
+}
+drawScreen();
